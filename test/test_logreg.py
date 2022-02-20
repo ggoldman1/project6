d524cb872119e40ad5417a06e01444ce3f9d3532
Grant Goldman <grant.goldman@ucsf.edu>
2022-02-20 14:20:51 -0800

    update tests

--- test/test_logreg.py
+++ test/test_logreg.py
@@ -6,6 +6,9 @@ Write your logreg unit tests here. Some examples of tests we will be looking for
 More details on potential tests below, these are not exhaustive
 """
 
+import regression
+import numpy as np
+
 def test_updates():
 	"""
 
@@ -15,16 +18,17 @@ def test_updates():
 	# Check that your loss function is correct and that 
 	# you have reasonable losses at the end of training
 
-	X_train, X_test, y_train, y_test = utils.loadDataset(
+
+	X_train, X_test, y_train, y_test = regression.utils.loadDataset(
 		features=['Penicillin V Potassium 500 MG', 'Computed tomography of chest and abdomen',
-				  -                          'Plain chest X-ray (procedure)', 'Low Density Lipoprotein Cholesterol',
-				  -                          'Creatinine'], split_percent=0.8)
-	lr = LogisticRegression(X_train.shape[1])
+				                          'Plain chest X-ray (procedure)', 'Low Density Lipoprotein Cholesterol',
+				                            'Creatinine'], split_percent=0.8)
+	lr = regression.LogisticRegression(X_train.shape[1])
 	lr.train_model(X_train, y_train, X_test, y_test)
 
 	## Test 1: check that loss is generally decreasing over iterations
-	halfway = int(len(lr.loss_train_history) / 2)
-	assert np.mean(lr.loss_train_history[0:halfway]) > np.mean(lr.loss_train_history[halfway:halfway*2])
+	halfway = int(len(lr.loss_history_train) / 2)
+	assert np.mean(lr.loss_history_train[0:halfway]) > np.mean(lr.loss_history_train[halfway:halfway*2])
 
 	## Test 2: 
 
