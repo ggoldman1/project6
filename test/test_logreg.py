09cdfc4554439bad335447ad3ad69a5ac5884d9b
Grant Goldman <grant.goldman@ucsf.edu>
2022-02-20 14:45:44 -0800

    more tests

--- test/test_logreg.py
+++ test/test_logreg.py
@@ -21,8 +21,8 @@ def test_updates():
 
 	X_train, X_test, y_train, y_test = regression.utils.loadDataset(
 		features=['Penicillin V Potassium 500 MG', 'Computed tomography of chest and abdomen',
-				                          'Plain chest X-ray (procedure)', 'Low Density Lipoprotein Cholesterol',
-				                            'Creatinine'], split_percent=0.8)
+			      'Plain chest X-ray (procedure)', 'Low Density Lipoprotein Cholesterol',
+                  'Creatinine'], split_percent=0.8)
 	lr = regression.LogisticRegression(X_train.shape[1])
 	lr.train_model(X_train, y_train, X_test, y_test)
 
@@ -30,7 +30,17 @@ def test_updates():
 	halfway = int(len(lr.loss_history_train) / 2)
 	assert np.mean(lr.loss_history_train[0:halfway]) > np.mean(lr.loss_history_train[halfway:halfway*2])
 
-	## Test 2: 
+	## Test 2: check that loss is low (below 1)
+    assert lr.loss_train_history[len(lr.loss_train_history)-1] < 1 
+
+    ## Test 3: check that loss gets lower if we allow more iterations
+    num_iter = [10, 100, 1000, 10000]
+    prev = np.inf 
+    for iter in num_iter:
+        lr = LogisticRegression(X_train.shape[1])
+        lr.train_model(X_train, y_train, X_test, y_test)
+        assert lr.loss_train_history[len(lr.loss_train_history)-1] < prev
+        prev = lr.loss_train_history[len(lr.loss_train_history)-1]  
 
 def test_predict():
 	# Check that self.W is being updated as expected
